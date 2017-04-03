d091439cbd2508e977b62d73f256c81ea2bd2514
LydiaSB <lydia.souriot@outlook.fr>
2017-04-03 17:42:26 +0200

    cc

--- Solver/Algorithms/Algorithms.cpp
+++ Solver/Algorithms/Algorithms.cpp
@@ -2,6 +2,7 @@
 
 #include <glpk.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 
 using namespace std;
--- Solver/Algorithms/Algorithms.hpp
+++ Solver/Algorithms/Algorithms.hpp
@@ -7,7 +7,7 @@ void glouton_VC(Graph &g);
 
 void IPL_VC(Graph &g);
 
-void ARB_VC(Graph &g);
+int ARB_VC(Graph &g);
 
 void KERNEL_VC(Graph &g);
 
